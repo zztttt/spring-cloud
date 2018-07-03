3e27f0bea11fce1d0a004294793ba88adb0fc75c
zzt <1315691850@qq.com>
2018-07-03 17:11:19 +0800

    update service-ribbon with Hystrix

--- service-ribbon/src/main/java/com/example/demo/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/com/example/demo/ServiceRibbonApplication.java
@@ -2,13 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
+@EnableCircuitBreaker
 public class ServiceRibbonApplication {
 
 	public static void main(String[] args) {
@@ -16,6 +20,7 @@ public class ServiceRibbonApplication {
 	}
 	@Bean
     @LoadBalanced
+    
     RestTemplate restTemplate() {
         return new RestTemplate();
     }
